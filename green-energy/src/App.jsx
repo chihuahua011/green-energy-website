dd261f9a8197e2b73ba18577a73c5782ed7d8310
Marcell Monoki <83634452+chihuahua011@users.noreply.github.com>
2024-03-21 16:29:38 +0100

    Hero component base done

--- green-energy/src/App.jsx
+++ green-energy/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import Hero from './components/Hero/Hero';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Hero></Hero>
+      <Footer></Footer>
     </div>
   );
 }
--- green-energy/src/components/Hero/Hero.jsx
+++ green-energy/src/components/Hero/Hero.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
+import Button from "../Button/Button";
 import "./hero.scss";
 
 function Hero() {
-  return (
-    <>
-        <div>Hero</div>
-    </>
-  )
+    return (
+        <>
+            <span className="backgrnd-img"></span>
+            <section className="hero pt-8">
+				<div className="container p-def">
+					<div className="middle-cont col justify-center align-center">
+						<div className="card px-7 py-2 mt-5 justify-center align-center">
+							<p>Discover Sustainable Solutions</p>
+							<p>for a Greener Tomorrow</p>
+							<Button className={"button primary mt-2"}>Let's talk</Button>
+						</div>
+					</div>
+					<div className="hero-text col pb-2 justify-right">
+						<h1>Empowering</h1>
+						<h1>Green Futures</h1>
+						<h4>Harnessing the Power of Renewable Energy</h4>
+					</div>
+					<span className="white-rect"></span>
+				</div>
+            </section>
+        </>
+    );
 }
 
-export default Hero
+export default Hero;
