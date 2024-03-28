10102fa85f596441cff762627694fa8ac28a17d5
Marcell Monoki <83634452+chihuahua011@users.noreply.github.com>
2024-03-28 15:06:16 +0100

    Squashed commit of the following:
    commit 2e416524b0243dc2c77439f23b10944e734485e5
    Author: Marcell Monoki <83634452+chihuahua011@users.noreply.github.com>
    Date:   Thu Mar 28 15:01:07 2024 +0100
    
        patch version update
    
    commit 6f1c0de5731e5166c3d0f5147fde2b182348af3c
    Author: Marcell Monoki <83634452+chihuahua011@users.noreply.github.com>
    Date:   Thu Mar 28 15:00:14 2024 +0100
    
        Added icons & favicon
    
    commit 66a1d0b72c2757ce817807c68f1ebc40e36278ac
    Author: chihuahua011 <monoki07@gmail.com>
    Date:   Wed Mar 27 23:25:41 2024 +0100
    
        gh-pages install
    
    commit 29705ef82104ad73c91267d052a421aa04e8ff0e
    Author: Marcell Monoki <83634452+chihuahua011@users.noreply.github.com>
    Date:   Wed Mar 27 14:00:43 2024 +0100
    
        update README

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Creating this project for the purpouse of practice and portfolio piece
 
 ## Bugs & Fixes
+Right now the page aren't really responsive, will make it compatible with the most common screen sizes
+although the mobile version will have to wait.
--- green-energy/src/components/Footer/Footer.jsx
+++ green-energy/src/components/Footer/Footer.jsx
@@ -16,6 +16,7 @@ function Footer() {
 							<Button className={"link"}><p>FAQ</p></Button>
 						</div>
 					</div>
+					<div className="flaticon"> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div>
 				</div>
             </section>
         </>
